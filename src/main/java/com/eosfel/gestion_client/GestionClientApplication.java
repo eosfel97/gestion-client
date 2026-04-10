0ce228b3f73c38c395c60aefa356b133180bb9af
Gnahiet.any <any.gouba@gmail.com>
2026-04-10 22:59:36 +0200

    Premier commit - Ajout du projet

---
+++ src/main/java/com/eosfel/gestion_client/GestionClientApplication.java
@@ -0,0 +1,13 @@
+package com.eosfel.gestion_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eosfel/gestion_client/GestionClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eosfel.gestion_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
